5ed3895748bf2cf979681397387857cef26a6434
jackdoe13 <81348141+jackdoe13@users.noreply.github.com>
2025-12-19 14:31:42 +0900

    [level 0] Title: 양꼬치, Time: 0.23 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        if(n >= 10)
+        {
+            k = k - (n/10);
+        }
+        
+        
+        answer = (n*12000) + (k*2000);
+        
+        return answer;
+    }
+}
